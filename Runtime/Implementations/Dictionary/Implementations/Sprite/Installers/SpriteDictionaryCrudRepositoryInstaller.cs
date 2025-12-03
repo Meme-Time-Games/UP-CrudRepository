b7f1ca0649d6607f0753d3317a5ba4c78d5c0ca9
Fricodev <fede97cem@gmail.com>
2025-12-03 17:27:25 -0300

    [A] Added Sprite Dictionary Crud Repository implementation

---
+++ Runtime/Implementations/Dictionary/Implementations/Sprite/Installers/SpriteDictionaryCrudRepositoryInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using CrudRepository.Core;
+using CrudRepository.Implementations;
+using UnityEngine;
+
+namespace CrudRepository.Installers
+{
+        public class SpriteDictionaryCrudRepositoryInstaller : DictionaryCrudRepositoryInstaller<int, Sprite>
+        {
+            [SerializeField] private List<Sprite> _sprites;
+        
+            protected override ICrudRepository<int, Sprite> GetData()
+            {
+                DictionaryCrudRepository<int, Sprite> dictionaryCrudRepository = new DictionaryCrudRepository<int, Sprite>();
+
+                for (int i = 0; i < _sprites.Count; i++)
+                {
+                    dictionaryCrudRepository.Insert(i, _sprites[i]);
+                }
+
+                return dictionaryCrudRepository;
+            }
+        }
+}
